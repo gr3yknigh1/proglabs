508a167cee75718441e0a896e2c610c7c767c5f0
Ilya Akkuzin <gr3yknigh1@gmail.com>
2022-09-29 18:36:38 +0300

    Add lab 1

---
+++ lab01.c
@@ -0,0 +1,28 @@
+/*
+ * Лабараторная №1
+ * Аккузин Илья (ПО-2022-1)
+ * 
+ * NOTE: Добавьте флаг `-lm` чтобы включить `math.h`
+ * $ gcc lab01.c -o lab01 -lm
+ * */
+#include <stdio.h>
+#include <math.h>
+
+double f(double x) {
+    return sqrt(
+        (2 * x + sin(fabs(3 * x))) / 3.56
+    );
+}
+
+int main() {
+
+    double x;
+    printf("x: ");
+    scanf("%lf", &x);
+    double y = f(x);
+    printf("y: %lf", y);
+
+    return 0;
+}
+
+
