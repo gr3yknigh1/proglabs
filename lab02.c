e1422170bb84cadb9ee03b21332ef7a060ae55d3
Ilya Akkuzin <gr3yknigh1@gmail.com>
2022-09-30 11:19:20 +0300

    Add lab02

---
+++ lab02.c
@@ -0,0 +1,36 @@
+/*
+ * Лабараторная №2
+ * Аккузин Илья (ПО-2022-1)
+ * 
+ * $ gcc lab02.c -o lab02
+ * */
+
+#include <stdio.h>
+
+
+int main() {
+
+    char a = 'A';
+    printf("%c\n", a);
+
+    char name[15];
+    printf("Меня зовут ");
+    scanf("%s", name);
+
+    int x = 315;
+    printf("%d\n", x);
+
+    int y = -27;
+    printf("%d\n", y);
+
+    float z = 127.361;
+    printf("%f\n", z);
+
+    double j = -574.953;
+    printf("%lf\n", j);
+
+    long h = 2124;
+    printf("%ld\n", h);
+
+    return 0;
+}
